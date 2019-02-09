f2dc0c03d529d5c18121b6a84a9cc5c78bc95e9b
joakim1601 <joakim.granstrom.1601@student.uu.se>
2019-02-09 15:39:11 +0100

    Implemented a temporary wordCheck counter

--- vue.js
+++ vue.js
@@ -1,13 +1,14 @@
-var vm = new Vue ({
+var vm = new Vue({
     el: '#inputForm',
     data: {
         inputString: '',
         inputData: [],
+        wordCompare: [],
     },
     methods: {
-        prependWord: function(inputString) {
+        prependWord: function (inputString) {
             vm.inputData.push(this.inputString);
-            words.words1.pop();
+            vm.wordCompare.push(words.words1.pop());
             this.textField = '';
             this.inputString = '';
             this.$refs.textField.focus();
@@ -15,13 +16,13 @@ var vm = new Vue ({
     }
 });
 
-var words = new Vue ({
+var words = new Vue({
     el: '#words',
     data: {
         words1: ["testar", "mitt", "program"],
     },
     methods: {
-        reverseArray: function(array) {
+        reverseArray: function (array) {
             return array.slice().reverse();
         }
     }
@@ -29,19 +30,24 @@ var words = new Vue ({
 });
 
 
-/*
-* Check if inputData == words1
-var wordCheck = new Vue ({
+var wordCheck = new Vue({
     el: '#wordCheck',
     data: {
         checkTrue: 'YES',
         checkFalse: 'NO',
+        wordCountTemp: 0,
+        wordCount: 0,
 
     },
     methods: {
-        arrayCheck: function() {
-          for (var i = 0)
+        arrayCheck: function () {
+            for (var i = 0; i < vm.inputData.length; i++) {
+                if (vm.inputData[i] == vm.wordCompare[vm.wordCompare.length - 1 - i]) {
+                    this.wordCount += 1;
+                }
+            }
+
+            //this.wordCount = this.wordCountTemp;
         }
     },
 });
-*/
